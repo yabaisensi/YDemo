7b6e7cde7575784faaccb6f1377a7b0e4606a42d
DESKTOP-2VQNNP9\administrator <demmy>
2023-03-11 00:58:24 +0900

    Saved on 周六 2023/03/11  0:58:24.74 by xu version 19

---
+++ src/main/java/test/TestSnack.java
@@ -0,0 +1,45 @@
+package test;
+
+import java.util.Scanner;
+
+public class TestSnack {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuffer sb = new StringBuffer();
+		if(!sc.hasNextLine()) System.exit(0);
+		System.out.println("SCCCCCC "+sc.nextLine()); 
+		if(!sc.hasNextLine()) System.exit(0);
+		System.out.println("SCCCCCC "+sc.nextLine()); 
+		if(!sc.hasNextLine()) System.exit(0);
+		System.out.println("SCCCCCC "+sc.nextLine()); 
+		if(!sc.hasNextLine()) System.exit(0);
+		System.out.println("SCCCCCC "+sc.nextLine()); 
+		if(!sc.hasNextLine()) System.exit(0);
+		if(!sc.hasNextLine()) System.exit(0);
+		if(!sc.hasNextLine()) System.exit(0);
+		if(!sc.hasNextLine()) System.exit(0);
+		if(!sc.hasNextLine()) System.exit(0);
+//		nextStr.
+//		while(sc.hasNextLine()) {
+//			String nextStr =  sc.nextLine();
+//			while (nextStr.contains("_")) {
+//				nextStr = nextStr.replaceFirst("_[a-z]",
+//					String.valueOf(Character.toUpperCase(nextStr.charAt(nextStr.indexOf("_") + 1))));
+//			}
+//			sb.append(nextStr+"\r\n");
+//		}
+		System.out.println(sb);
+		System.out.println("END END");
+		
+		
+//	    String cstr = sc.next();
+//		String phrase = "always_use_camel_back_notation_in_java";
+//		while (phrase.contains("_")) {
+//			phrase = phrase.replaceFirst("_[a-z]",
+//					String.valueOf(Character.toUpperCase(phrase.charAt(phrase.indexOf("_") + 1))));
+//		}
+//		System.out.println(phrase);
+	}
+
+}
