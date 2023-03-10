ccc984ca21d612da7f5fdf4aa7ab8d46d0fe4439
DESKTOP-2VQNNP9\administrator <demmy>
2023-03-11 02:45:19 +0900

    Saved on 周六 2023/03/11  2:45:19.53 by xu version 21

--- src/main/java/test/TestSnack.java
+++ src/main/java/test/TestSnack.java
@@ -1,13 +1,23 @@
 package test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class TestSnack {
-
-	public static void main(String[] args) {
+	private static final String IN_FILE = "C:\\Users\\Administrator\\Documents\\pleiades-2021-12-java-win-64bit-jre_20220106\\pleiades\\workspace\\YDemo\\src\\main\\java\\test\\snack_in.txt";
+	private static final String OUT_FILE = "C:\\Users\\Administrator\\Documents\\pleiades-2021-12-java-win-64bit-jre_20220106\\pleiades\\workspace\\YDemo\\src\\main\\java\\test\\snack_out.txt";
+	public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         StringBuffer sb = new StringBuffer();
         try {
+        	ProcessBuilder pb = new ProcessBuilder("cmd");
+        	Runtime rt = Runtime.getRuntime();
+        	rt.exec("notepad \""+IN_FILE+"\"");
+        	System.out.println();
+        	new java.util.Scanner(System.in).nextLine();
             while (scan.hasNextLine()){
 
             	String line = scan.nextLine();
@@ -23,8 +33,17 @@ public class TestSnack {
                 sb.append(line);
             }
         } finally {
-        	System.out.println(sb);
-            scan.close();
+        	ProcessBuilder pb = new ProcessBuilder("cmd");
+        	scan.close();
+			Runtime rt = Runtime.getRuntime();
+			rt.exec("pause");
+			BufferedWriter bwr = new BufferedWriter(new FileWriter(new File(OUT_FILE)));
+			bwr.write(sb.toString());
+			bwr.flush();
+			bwr.close();
+			pb.start();
+		    String staStr = "notepad \""+OUT_FILE+"\"";
+			rt.exec(staStr);
         }
 	}
 
