48ba86fbf55524f4813f14514c5018a5c5b121cf
DESKTOP-2VQNNP9\administrator <demmy>
2023-03-11 14:52:45 +0900

    Saved on 周六 2023/03/11 14:52:45.32 by xu version 26

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,26 +1,21 @@
 package test;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
 
 public class Test {
 
     	public static void main(String[] args) throws InterruptedException, IOException, URISyntaxException {
-    		int index=0;
-    		while(index<4) {
-    			index++;
-    			Scanner sc = new Scanner(System.in);
-    			String cstr = sc.nextLine();
-    			cstr=cstr.replaceAll("\"", "");
-    			Path path = Paths.get(cstr);
-    			String pstr = path.getParent().toString();
-    			ProcessBuilder pb = new ProcessBuilder("cmd");
-    			pb.start();
-    			Runtime rt = Runtime.getRuntime();
-    			rt.exec("explorer "+ pstr);
-    		}
+    			try {
+					Robot robot = new Robot();
+					// ctrl
+					robot.keyPress(KeyEvent.VK_CONTROL);
+					System.out.println("KeyEvent.VK_CONTROL"+ KeyEvent.VK_CONTROL);
+				} catch (AWTException e) {
+					e.printStackTrace();
+				}
   }
 }
