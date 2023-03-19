0277a2fc8a58e45279f305852522f727e9eea7f3
DESKTOP-2VQNNP9\administrator <demmy>
2023-03-19 09:02:29 +0900

    Saved on 周日 2023/03/19  9:02:29.09 by xu version 27

--- src/main/java/test/TestSnack3.java
+++ src/main/java/test/TestSnack3.java
@@ -1,32 +1,32 @@
-package test;
-
-import java.io.IOException;
-import java.util.Map;
-
-public class TestSnack3 {
-	public static void main(String[] args) throws IOException {
-        try {
-//        	ProcessBuilder pb = new ProcessBuilder("cmd.exe explorer");
-//        	pb.start();
-        	 ProcessBuilder pb =
-        			   new ProcessBuilder("myCommand", "myArg1", "myArg2");
-        			 Map<String, String> env = pb.environment();
-        			 env.put("VAR1", "myValue");
-        			 env.remove("OTHERVAR");
-        			 env.put("VAR2", env.get("VAR1") + "suffix");
-        			 pb.directory(new File("myDir"));
-        			 File log = new File("log");
-        			 pb.redirectErrorStream(true);
-        			 pb.redirectOutput(Redirect.appendTo(log));
-        			 Process p = pb.start();
-        			 assert pb.redirectInput() == Redirect.PIPE;
-        			 assert pb.redirectOutput().file() == log;
-        			 assert p.getInputStream().read() == -1;
-//        	Runtime rt = Runtime.getRuntime();
-//        	rt.exec(new String[]{"explorer"});
-//        	rt.exec(new String[]{"cmd.exe","/c","start excel.exe"});
-        } finally {
-        }
-	}
-
-}
+//package test;
+//
+//import java.io.IOException;
+//import java.util.Map;
+//
+//public class TestSnack3 {
+//	public static void main(String[] args) throws IOException {
+//        try {
+////        	ProcessBuilder pb = new ProcessBuilder("cmd.exe explorer");
+////        	pb.start();
+//        	 ProcessBuilder pb =
+//        			   new ProcessBuilder("myCommand", "myArg1", "myArg2");
+//        			 Map<String, String> env = pb.environment();
+//        			 env.put("VAR1", "myValue");
+//        			 env.remove("OTHERVAR");
+//        			 env.put("VAR2", env.get("VAR1") + "suffix");
+//        			 pb.directory(new File("myDir"));
+//        			 File log = new File("log");
+//        			 pb.redirectErrorStream(true);
+//        			 pb.redirectOutput(Redirect.appendTo(log));
+//        			 Process p = pb.start();
+//        			 assert pb.redirectInput() == Redirect.PIPE;
+//        			 assert pb.redirectOutput().file() == log;
+//        			 assert p.getInputStream().read() == -1;
+////        	Runtime rt = Runtime.getRuntime();
+////        	rt.exec(new String[]{"explorer"});
+////        	rt.exec(new String[]{"cmd.exe","/c","start excel.exe"});
+//        } finally {
+//        }
+//	}
+//
+//}
